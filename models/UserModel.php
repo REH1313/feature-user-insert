6c16319c0eb06a374c0843cdab62aa7a4b690e7f
Rebeca <rebhernandez144@gmail.com>
2025-11-09 21:01:12 -0600

    Update UserModel.php

--- models/UserModel.php
+++ models/UserModel.php
@@ -4,9 +4,6 @@ require_once 'db.php';
 function create_user($username, $password) {
     global $pdo;
     $stmt = $pdo->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
-    $stmt->execute([
-        ':username' => $username,
-        ':password' => $password
-    ]);
+    $stmt->execute([':username' => $username,':password' => $password]);
     return $pdo->lastInsertId();
 }
