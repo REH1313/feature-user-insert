2f7a8cfc18fd050108e2bcb3bb70b7cfdda9a108
Rebeca <rebhernandez144@gmail.com>
2025-12-06 12:09:16 -0600

    Update edit.php

--- views/profile/edit.php
+++ views/profile/edit.php
@@ -3,6 +3,16 @@
 <form method="POST" action="controllers/UserController.php?action=update" class="card card-body mt-4">
   <h5 class="card-title">Edit Profile</h5>
   
+    <input type="hidden" name="id" value="<?= htmlspecialchars($user['id']); ?>">
+
+    <?php include 'views/profile/partials/form-fields.php'; ?>
+
+    <div class="mt-3">
+      <button type="submit" class="btn btn-success">Save Changes</button>
+      <a href="profile.php?id=<?= htmlspecialchars($user['id']); ?>" class="btn btn-secondary">Cancel</a>
+    </div>
+</form>
+
   <?php 
     // Reuse the shared partial for username/password fields
     include 'views/profile/partials/form-fields.php'; 
